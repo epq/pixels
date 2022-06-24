a3089962ed3f685285d08617c64f41c0b851be95
challah <yang.bread@gmail.com>
2022-06-23 22:24:49 -0400

    rename files to include year

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // old script to populate the gallery; re-written in Pug
 
 const filesList = [
-  { image: "03.png", name: "Milky" },
-  { image: "04.gif", name: "Cow" },
-  { image: "05.png", name: "Tortoiseshell" },
-  { image: "06.gif", name: "Qiu" },
-  { image: "07.gif", name: "Torus" },
-  { image: "08.png", name: "Worm" },
-  { image: "09.gif", name: "Tomat" },
-  { image: "10.gif", name: "Ghosty" },
-  { image: "11.gif", name: "Frog" },
-  { image: "12.gif", name: "Potato" },
-  { image: "13.gif", name: "Geometry" },
-  { image: "14.gif", name: "String" },
-  { image: "15.gif", name: "Egg" },
+  { image: "21-03.png", name: "Milky" },
+  { image: "21-04.gif", name: "Cow" },
+  { image: "21-05.png", name: "Tortoiseshell" },
+  { image: "21-06.gif", name: "Qiu" },
+  { image: "21-07.gif", name: "Torus" },
+  { image: "21-08.png", name: "Worm" },
+  { image: "21-09.gif", name: "Tomat" },
+  { image: "21-10.gif", name: "Ghosty" },
+  { image: "21-.gif", name: "Frog" },
+  { image: "21-.gif", name: "Potato" },
+  { image: "21-.gif", name: "Geometry" },
+  { image: "21-.gif", name: "String" },
+  { image: "21.gif", name: "Egg" },
 ];
 
 const gallery = document.querySelector(".gallery");
